da0b43484914db5b79cab91cd34052a6ae9a0c73
niexiaobo <flash_lifing@163.com>
2016-08-12 21:12:48 +0800

    我的日常开发笔记
    我的日常开发笔记

---
+++ MyDailyDevelopmentNotes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyDailyDevelopmentNotes
+//
+//  Created by beyondsoft-聂小波 on 16/8/12.
+//  Copyright © 2016年 NieXiaobo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyDailyDevelopmentNotes/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MyDailyDevelopmentNotes
+//
+//  Created by beyondsoft-聂小波 on 16/8/12.
+//  Copyright © 2016年 NieXiaobo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
